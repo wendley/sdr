a82bee82736a1dd627e367a5d88f5b615f209a63
Wendley S. Silva <wendley@ufc.br>
2018-08-15 14:48:46 -0300

    atualizacao

---
+++ clone.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Shell script para atualizar o arquivo getRSSI.py do modulo gr-LQE
+# Wendley S. Silva – wendley@gmail.com - Jul/2018
+
+
+cd ~/ ;
+rm sdr/ -rf ;
+git clone https://github.com/wendley/sdr.git ;
+cd sdr;
+cp * ~/ ;
+cd ~;
