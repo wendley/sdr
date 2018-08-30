3d8a94b87d4e3e77417f37fe2379aceddcf4962c
Wendley S. Silva <wendley@ufc.br>
2018-08-30 16:29:12 -0300

    atualicacao de 30/08/2018

--- getRSSI.py
+++ getRSSI.py
@@ -460,7 +460,7 @@ class getRSSI(gr.sync_block):
 				self.finalSerieML = numpy.arange(2).reshape(1,-1)
 				estimSVMR = self.clf.predict(self.finalSerieML) # Predizer somente o ultimo valor da serie
 				erroSVMR = estimSVMR - self.serieTarget[-1]
-				# print "ESTIMATIVA GERADA PELA ML-SVMR: %f" %estimSVMR
+				print "ESTIMATIVA GERADA PELA ML-SVMR: %f" %estimSVMR
 				# print "ERRO do SVMR: %f" %erroSVMR
 
 
