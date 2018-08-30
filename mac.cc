fac9e67196976106ca7d7a64c8c8600fe45a8e2e
Wendley S. Silva <wendley@ufc.br>
2018-08-30 13:20:58 -0300

    atualicacao de 30/08/2018

--- mac.cc
+++ mac.cc
@@ -240,11 +240,11 @@ public:
             dout << "MAC: exiting in few seconds" << std::endl;
             message_port_pub(pmt::mp("ackOut"), pmt::from_long(999)); //999 sinaliza fim de transmissão
             endOfFile = true;
-            block::stop();
+            exec->interrupt();
             if (!data_ready){
                 message_port_pub(pmt::mp("ackOut"), pmt::from_long(999)); //999 sinaliza fim de transmissão
                 detail().get()->set_done(true);
-                block::stop();
+                exec->interrupt();
             }
             // detail().get()->set_done(true);
             return;
