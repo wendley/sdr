30b3383d97ebe9e4159f58bb4bfd23c34b002915
Wendley S. Silva <wendley@ufc.br>
2019-06-17 17:33:37 -0300

    atualizacao de 17/06/2019

--- getRSSI.py
+++ getRSSI.py
@@ -151,6 +151,8 @@ class getRSSI(gr.sync_block):
 		self.reg = linear_model.BayesianRidge() # LQL
 
 		self.clf = joblib.load('fileTrain.joblib')
+		self.reg = joblib.load('fileTrainLQL.joblib')
+
 		self.profund.append(self.clf.tree_.max_depth)
 
 		self.startT = time.time()
@@ -598,21 +600,25 @@ class getRSSI(gr.sync_block):
 			self.finalSerieLQL = numpy.array(self.serieLQL)
 
 
-			if len(self.serieLQL) >= 20:
-				self.finalSerieLQL=numpy.array(self.serieLQL)
+			# if len(self.serieLQL) >= 20: # Somente usado para treinar a primeira vez
+			# 	self.finalSerieLQL=numpy.array(self.serieLQL)
 
-				if self.treinar == True :
-					self.contaTreinos +=1
-					self.reg.fit(self.serieLQL[:-1],self.serieTargetLQL[:-1]) # Treina com todos os dados da serie, exceto o último
-				self.finalSerieLQL = self.serieLQL[-1]
-				self.finalSerieLQL = numpy.arange(2).reshape(1,-1) # Para duas entradas, usar 	self.finalSerieML = numpy.arange(2).reshape(1,-1)
-				self.estimSVMRLQL = float(self.reg.predict(self.finalSerieLQL)) # Predizer somente o ultimo valor da serie
-				erroSVMRLQL = numpy.abs(self.estimSVMRLQL - self.serieTargetLQL[-1])
-				self.serieErroSVMRLQL.append(erroSVMRLQL)
+			# 	if self.treinar == True :
+			# 		self.contaTreinos +=1
+			# 		self.reg.fit(self.serieLQL[:-1],self.serieTargetLQL[:-1]) # Treina com todos os dados da serie, exceto o último
+			# 	self.finalSerieLQL = self.serieLQL[-1]
+			# 	self.finalSerieLQL = numpy.arange(2).reshape(1,-1) # Para duas entradas, usar 	self.finalSerieML = numpy.arange(2).reshape(1,-1)
+			# 	self.estimSVMRLQL = float(self.reg.predict(self.finalSerieLQL)) # Predizer somente o ultimo valor da serie
+			# 	erroSVMRLQL = numpy.abs(self.estimSVMRLQL - self.serieTargetLQL[-1])
+			# 	self.serieErroSVMRLQL.append(erroSVMRLQL)
 
-				self.timestr = time.strftime("%Y%m%d-%H%M%S") # Se quiser salvar o arquivo de treinamento .joblib
-    			filename = "fileTrainLQL"+self.timestr+".joblib"
-    			joblib.dump(self.reg,filename)
+			# 	self.timestr = time.strftime("%Y%m%d-%H%M%S") # Se quiser salvar o arquivo de treinamento .joblib
+    		#	filename = "fileTrainLQL"+self.timestr+".joblib"
+    		#	joblib.dump(self.reg,filename)
+
+    		self.finalSerieLQL = self.serieLQL[-1]
+			self.finalSerieLQL = numpy.arange(2).reshape(1,-1) # Para duas entradas, usar 	self.finalSerieML = numpy.arange(2).reshape(1,-1)
+    		self.estimSVMRLQL = float(self.reg.predict(self.finalSerieLQL))
 
 
 			self.message_port_pub(pmt.intern("estimation"),pmt.from_double(self.estimSVMRLQL))
